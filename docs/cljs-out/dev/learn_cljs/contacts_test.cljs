7ac06233acf38d8824669005834e02045419105e
Anton Logvinenko <anton.logvinenko@gmail.com>
2025-03-30 14:23:53 +0300

    Added content

--- docs/cljs-out/dev/learn_cljs/contacts_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns learn-cljs.contacts-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [learn-cljs.contacts]))
-
--- docs/cljs-out/dev/learn_cljs/contacts_test.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.11.132 {:optimizations :none}
-goog.provide('learn_cljs.contacts_test');
-goog.require('cljs.core');
-goog.require('cljs.test');
-goog.require('learn_cljs.contacts');
-
-//# sourceMappingURL=contacts_test.js.map
