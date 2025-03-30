244de90f212f694d17518985dbba631df55d86e7
Anton Logvinenko <anton.logvinenko@gmail.com>
2025-03-30 14:58:17 +0300

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 set -ex
+
+git stash
+
 clj -M:fig:min
 rm -Rf docs/*
 cp -r resources/public/css docs/
 cp -r resources/public/fonts docs/
 mkdir docs/cljs-out
 cp target/public/cljs-out/dev-main.js docs/cljs-out/
+
+git add docs
+git commit -m "Deploying a new version at `date`"
+git push origin main
+
+git stash pop
