458b80cf5d79a5d85274242ccc35494c906bd0ef
Anton Logvinenko <anton.logvinenko@gmail.com>
2025-03-30 10:34:54 +0300

    Cleanup

--- contacts/src/learn_cljs/contacts.cljs
+++ contacts/src/learn_cljs/contacts.cljs
@@ -5,9 +5,13 @@
             [goog.events :as gevents]
             [clojure.string :as str]))
 
-;; Models and State
 
-(def contact-list [])
+
+;; Model
+(def initial-state
+  {:contacts []
+   :selected nil
+   :editing? false})
 
 (defn make-address [address]
   (select-keys address [:street :city :state :postal :country]))
@@ -23,8 +27,7 @@
       (maybe-set-address)))
 
 (defn add-contact [contact-list input]
-  (conj contact-list
-        (make-contact input)))
+  (conj contact-list (make-contact input)))
 
 (defn replace-contact [contact-list idx input]
   (assoc contact-list idx (make-contact input)))
@@ -33,113 +36,12 @@
   (vec (concat (subvec contact-list 0 idx)
                (subvec contact-list (inc idx)))))
 
-;; UI
 
-(def app-container (gdom/getElement "app"))
+
+;; UI behavior
 (declare refresh!)
 
-(def top-bar
-  [:div {:class "navbar has-shadow"}
-   [:div {:class "container"}
-    [:div {:class "navbar-brand"}
-     [:span {:class "navbar-item"}
-      "ClojureScript Contacts"]]]])
-
-(defn action-button [id text icon-class]
-  [:button {:id id
-            :class "button is-primary is-light"}
-   [:span {:class (str "mu " icon-class)}]
-   (str " " text)])
-
-(def save-button (action-button "save-contact" "Save" "mu-file"))
-(def cancel-button (action-button "cancel-edit" "Cancel" "mu-cancel"))
-(def add-button (action-button "add-contact" "Add" "mu-plus"))
-
-(defn section-header [editing?]
-  [:div {:class "section-header"}
-   [:div {:class "level"}
-    [:div {:class "level-left"}
-     [:div {:class "level-item"}
-      [:h1 {:class "subtitle"}
-       [:span {:class "mu mu-user"}]
-       "Edit Contact"]]]
-    [:div {:class "level-right"}
-     (if editing?
-       [:div {:class "buttons"} cancel-button save-button]
-       add-button)]]])
-
-(defn format-name [contact]
-  (->> contact
-       ((juxt :first-name :last-name))
-       (str/join " ")))
-
-(defn delete-icon [idx]
-  [:span {:class "delete-icon"
-          :data-idx idx}
-   [:span {:class "mu mu-delete"}]])
-
-(defn render-contact-list-item [idx contact selected?]
-  [:div {:class (str "card contact-summary" (when selected? " selected"))
-         :data-idx idx}
-   [:div {:class "card-content"}
-    [:div {:class "level"}
-     [:div {:class "level-left"}
-      [:div {:class "level-item"}
-       (delete-icon idx)
-       (format-name contact)]]
-     [:div {:class "level-right"}
-      [:span {:class "mu mu-right"}]]]]])
-
-(defn render-contact-list [state]
-  (let [contacts (:contacts state)
-        selected (:selected state)]
-    [:div {:class "contact-list column is-4 hero is-fullheight"}
-     (map-indexed (fn [idx contact]
-                    (render-contact-list-item idx contact (= idx selected)))
-                  contacts)]))
-
-(def no-contact-details
-  [:p {:class "notice"}
-   "No contact selected"])
-
-(defn form-field
-  ([id value label] (form-field id value label "text"))
-  ([id value label type]
-   [:div {:class "field"}
-    [:label {:class "label"} label]
-    [:div {:class "control"}
-     [:input {:id id
-              :value value
-              :type type
-              :class "input"}]]]))
-
-(defn render-contact-details [contact]
-  (let [address (get contact :address {})]
-    [:div {:id "contact-form" :class "contact-form"}
-     (form-field "input-first-name" (:first-name contact) "First Name")
-     (form-field "input-last-name" (:last-name contact) "Last Name")
-     (form-field "input-email" (:email contact) "Email" "email")
-     [:fieldset
-      [:legend "Address"]
-      (form-field "input-street" (:street address) "Street")
-      (form-field "input-city" (:city address) "City")
-      (form-field "input-state" (:state address) "State")
-      (form-field "input-postal" (:postal address) "Postal Code")
-      (form-field "input-country" (:country address) "Country")]]))
-
-(defn get-field-value [id]
-  (let [value (.-value (gdom/getElement id))]
-    (when (seq value) value)))
-
-(defn get-contact-form-data []
-  {:first-name (get-field-value "input-first-name")
-   :last-name (get-field-value "input-last-name")
-   :email (get-field-value "input-email")
-   :address {:street (get-field-value "input-street")
-             :city (get-field-value "input-city")
-             :state (get-field-value "input-state")
-             :postal (get-field-value "input-postal")
-             :country (get-field-value "input-country")}})
+(def app-container (gdom/getElement "app"))
 
 (defn set-app-html! [html-str]
   (set! (.-innerHTML app-container) html-str))
@@ -148,6 +50,20 @@
   (refresh! (-> state
                 (assoc :editing? true)
                 (dissoc :selected))))
+
+(defn get-field-value [id]
+  (let [value (.-value (gdom/getElement id))]
+    (when (seq value) value)))
+
+(defn get-contact-form-data []
+  {:first-name (get-field-value "input-first-name")
+   :last-name  (get-field-value "input-last-name")
+   :email      (get-field-value "input-email")
+   :address    {:street  (get-field-value "input-street")
+                :city    (get-field-value "input-city")
+                :state   (get-field-value "input-state")
+                :postal  (get-field-value "input-postal")
+                :country (get-field-value "input-country")}})
 
 (defn on-save-contact [state]
   (refresh!
@@ -165,7 +81,7 @@
   (refresh!
     (let [idx (int (.. e -currentTarget -dataset -idx))]
       (assoc state :selected idx
-                    :editing? true))))
+                   :editing? true))))
 
 (defn on-delete-contact [e state]
   (.stopPropagation e)
@@ -196,27 +112,117 @@
     (gevents/listen elem "click"
                     (fn [e] (on-delete-contact e state)))))
 
+
+
+;; UI rendering
+(defn action-button [id text icon-class]
+  [:button {:id    id
+            :class "button is-primary is-light"}
+   [:span {:class (str "mu " icon-class)}]
+   (str " " text)])
+
+(def save-button (action-button "save-contact" "Save" "mu-file"))
+(def cancel-button (action-button "cancel-edit" "Cancel" "mu-cancel"))
+(def add-button (action-button "add-contact" "Add" "mu-plus"))
+
+(defn section-header [editing?]
+  [:div {:class "section-header"}
+   [:div {:class "level"}
+    [:div {:class "level-left"}
+     [:div {:class "level-item"}
+      [:h1 {:class "subtitle"}
+       [:span {:class "mu mu-user"}]
+       "Edit Contact"]]]
+    [:div {:class "level-right"}
+     (if editing?
+       [:div {:class "buttons"} cancel-button save-button]
+       add-button)]]])
+
+(defn format-name [contact]
+  (->> contact
+       ((juxt :first-name :last-name))
+       (str/join " ")))
+
+(defn delete-icon [idx]
+  [:span {:class    "delete-icon"
+          :data-idx idx}
+   [:span {:class "mu mu-delete"}]])
+
+(defn render-contact-list-item [idx contact selected?]
+  [:div {:class    (str "card contact-summary" (when selected? " selected"))
+         :data-idx idx}
+   [:div {:class "card-content"}
+    [:div {:class "level"}
+     [:div {:class "level-left"}
+      [:div {:class "level-item"}
+       (delete-icon idx)
+       (format-name contact)]]
+     [:div {:class "level-right"}
+      [:span {:class "mu mu-right"}]]]]])
+
+(defn render-contact-list [state]
+  (let [contacts (:contacts state)
+        selected (:selected state)]
+    [:div {:class "contact-list column is-4 hero"}
+     (map-indexed
+       (fn [idx contact] (render-contact-list-item idx contact (= idx selected)))
+       contacts)]))
+
+(def no-contact-details
+  [:p {:class "notice"}
+   "No contact selected"])
+
+(defn form-field
+  ([id value label] (form-field id value label "text"))
+  ([id value label type]
+   [:div {:class "field"}
+    [:label {:class "label"} label]
+    [:div {:class "control"}
+     [:input {:id    id
+              :value value
+              :type  type
+              :class "input"}]]]))
+
+(defn render-contact-details [contact]
+  (let [address (get contact :address {})]
+    [:div {:id "contact-form" :class "contact-form"}
+     (form-field "input-first-name" (:first-name contact) "First Name")
+     (form-field "input-last-name" (:last-name contact) "Last Name")
+     (form-field "input-email" (:email contact) "Email" "email")
+     [:fieldset
+      [:legend "Address"]
+      (form-field "input-street" (:street address) "Street")
+      (form-field "input-city" (:city address) "City")
+      (form-field "input-state" (:state address) "State")
+      (form-field "input-postal" (:postal address) "Postal Code")
+      (form-field "input-country" (:country address) "Country")]]))
+
+(def top-bar
+  [:div {:class "navbar has-shadow"}
+   [:div {:class "container"}
+    [:div {:class "navbar-brand"}
+     [:span {:class "navbar-item"}
+      "ClojureScript Contacts"]]]])
+
 (defn render-app! [state]
   (set-app-html!
     (hiccups/html
-       [:div {:class "app-main"}]
-        top-bar
-        [:div {:class "columns"}]
-         (render-contact-list state)
-         [:div {:class "contact-details column is-8"}]
-          (section-header (:editing? state))
-          [:div {:class "hero is-fullheight"}]
-           (if (:editing? state)
-               (render-contact-details (get-in state [:contacts (:selected state)] {}))
-               no-contact-details))))
+      [:div {:class "app-main"}]
+      top-bar
+      [:div {:class "columns"}]
+      (render-contact-list state)
+      [:div {:class "contact-details column is-8"}]
+      (section-header (:editing? state))
+      [:div {:class "hero"}]
+      (if (:editing? state)
+        (render-contact-details (get-in state [:contacts (:selected state)] {}))
+        no-contact-details))))
 
+
+
+;; Running the whole app
 (defn refresh! [state]
   (render-app! state)
   (attach-event-handlers! state))
-
-(def initial-state
-  {:contacts contact-list
-   :selected nil
-   :editing? false})
 
 (refresh! initial-state)
