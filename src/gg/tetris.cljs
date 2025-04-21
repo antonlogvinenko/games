7c54544a8af74f86e0e7044bb15e6dc9b8365a98
Anton Logvinenko <anton.logvinenko@gmail.com>
2025-04-21 15:56:02 +0300

    the game is running

--- src/gg/tetris.cljs
+++ src/gg/tetris.cljs
@@ -58,6 +58,10 @@
    :y       height
    :element (random-element)
    :field   (vec (repeat height (vec (repeat width none))))})
+(defn show-field [field]
+  (log "The scene is:")
+  (->> field reverse (map (fn [r] (log r))) dorun)
+  field)
 
 
 ;; - Game UI
@@ -93,7 +97,7 @@
 (defn set-color [refs x y color]
   (gdom/setProperties
     (at refs x y)
-    #js {"style" (props {"background-color" color})}))
+    #js {"style" (props {"background-color" ({0 "white" 1 "black"} color)})}))
 
 (defn set-value [id value]
   (gdom/setTextContent (gdom/getElement id) value))
@@ -102,6 +106,7 @@
   (set-value "game-message" "The game is on"))
 
 (defn game-over-message []
+  (log "GAME OVER")
   (set-value "game-message" "Game over"))
 
 (defn set-game-html [html-str]
@@ -159,12 +164,13 @@
   (if (can-descend state)
     state
     (do (merge state {:field   (add-element-to-field state)
-                       :x       (/ width 2) :y height
-                       :element (random-element)}))))
+                      :x       (/ width 2) :y height
+                      :element (random-element)}))))
 ;; todo add test
 (defn game-over [{stop :stop :as state}]
   (stop)
-  (game-over-message))
+  (game-over-message)
+  state)
 ;; todo add test
 (defn descend [state]
   (update state :y dec))
@@ -177,16 +183,14 @@
 (def handlers
   {:descend descend-handler})
 (defn action-handler [state msg]
-  (log "This is inside action handler")
-  ;(->> state :field reverse (map (fn [r] (println r))) dorun)
   (let [new-state ((get handlers msg identity) state)]
-    (println (:x new-state) (:y new-state))
-    (->> new-state :field reverse (map (fn [r] (println r))) dorun)
+    (log "New coords" (:x new-state) (:y new-state))
     new-state))
 
 
 (defn render [last-displayed-state new-state]
-  {})
+  (show-field new-state)
+  (field-diff last-displayed-state new-state))
 
 (defn listen [down-listener up-listener]
   (gevents/listen js/document "keydown" down-listener)
@@ -252,6 +256,7 @@
           modifiers (filter #{"shift"} modifiers)
           pair [modifiers pressed]]
       (key-commands pair))))
+
 ;; -- Map of channels
 ;; kbd listener
 ;;    register what's pressed, e.g. [Shift + Enter]
@@ -274,16 +279,15 @@
 ;;
 ;;
 (defn generate-scene [state]
-  (add-element-to-field state))
+  (log "Generated scene")
+  (show-field (add-element-to-field state)))
+
 (defonce game (atom {:stop #()}))
 (defn stop! []
   ((:stop @game)))
 (defn start! [parameters]
   (stop!)
-  (let [refs (render-game! parameters)
-        state (init-state parameters refs)
-
-        timed-ch-ctrl (default-ch)
+  (let [timed-ch-ctrl (default-ch)
         timed-ch (create-timed-ch timed-ch-ctrl 1000)
         kbd-ch (create-kbd-ch)
         null-inbox (default-ch)
@@ -292,22 +296,30 @@
         renderer-calculator-ch (default-ch)
         action-ch (default-ch)
         scene-ch (default-ch)
-        chord-ch (default-ch)]
+        chord-ch (default-ch)
+
+        refs (render-game! parameters)
+        state (init-state parameters refs)
+        state (assoc state :stop (fn []
+                                   (->> [timed-ch-ctrl timed-ch kbd-ch chord-ch action-ch scene-ch
+                                         renderer-calculator-ch renderer-ch]
+                                        (map (fn [ch] (put! ch :quit)))
+                                        dorun)))]
 
     (game-started-message)
 
     (actor "renderer"
-           false
+           true
            renderer-ch
            null-inbox
            {}
-           (fn [{msg :msg}] {:msg {}}))
+           (fn [{cmds :msg}] (dorun (for [{x :x y :y color :color} cmds] (set-color (:refs state) x y color)))))
 
     (actor "renderer calculator"
            false
            renderer-calculator-ch
            renderer-ch
-           {}
+           (:field state)
            (fn [{last-displayed-state :state new-state-to-display :msg}]
              {:msg   (render last-displayed-state new-state-to-display)
               :state new-state-to-display}))
@@ -322,7 +334,7 @@
                {:msg scene})))
 
     (actor "action handler"
-           true
+           false
            action-ch
            scene-ch
            state
@@ -376,11 +388,16 @@
 
 
 
-;; 1. glue together to make game start, generate trivial block, merge it, generate new, until game is over
-;;
-;; more advanced figures
-;; other movements: left, right, turn left, turn right, complete
-;; pass "speed" as the parameter for run; color scheme
+;; 1. fix rendering of the grid (cells are erased)
+;; 2. actions: move left/right, rotate left/right, complete
+;; 3. all items
+;; 4. erasing filled horizontal blocks
+;; 5. speed as parameters
+;; 6. controls info
+;; 7. color schemes to choose
+;; 8. new game button
+;; 9. sounds
+
 ;;
 ;; stealing precaution: hostname and verify what is visible in the obfuscated code
 ;; domain name
