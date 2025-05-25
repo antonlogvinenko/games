7bf55785a9b2b918ac74aed84365059a7aabbaad
Anton Logvinenko <anton.logvinenko@gmail.com>
2025-05-25 18:02:45 +0300

    implementing different speeds

--- src/gg/tetris.cljs
+++ src/gg/tetris.cljs
@@ -15,6 +15,12 @@
   (when (and LOG *logging*)
     (println objs)))
 
+; https://tetris.wiki/Tetris_(NES)
+(def fps 60.0988)
+(def levels {0 48 1 43 2 38 3 33 4 28 5 23 6 18 7 13 8 8 9 6 10 5 11 5 12 5 13 4 14 4 15 4 16 3 17 3 18 3 19 2})
+(defn sleep-ms-at-level [level]
+  (* 1000 (/ (levels level) fps)))
+
 
 ;; -- Game state
 (defrecord FieldDiff [x y color])
